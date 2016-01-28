5497ae4411094ff58489e5e8befcf7f783c584f9
Retika Frey <retika@retikas-macbook-pro.local>
2016-01-28 10:15:45 -0700

    Initial Commit

---
+++ Food Tracker 2Tests/Food_Tracker_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Food_Tracker_2Tests.swift
+//  Food Tracker 2Tests
+//
+//  Created by Retika Kumar on 1/28/16.
+//  Copyright © 2016 kumar.retika. All rights reserved.
+//
+
+import XCTest
+@testable import Food_Tracker_2
+
+class Food_Tracker_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
